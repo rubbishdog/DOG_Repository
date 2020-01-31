fcd8d223f0d06c847b6f0c1307eeefac5b222215
rubbishdog <761586470@qq.com>
2020-01-31 14:40:23 +0800

    构造函数

---
+++ code/PointClass.cpp
@@ -0,0 +1,52 @@
+//构造函数示例
+#include<iostream>
+using namespace std;
+//点类的界面部分 
+class Point{
+	public:
+		Point(); //默认构造函数 
+		Point(int); //一个参数的构造函数 
+		Point(int,int); //两个参数的构造函数 
+		void displayxy(); 
+	private:
+		int X,Y;
+}; 
+//点类的实现部分 
+Point::Point() 
+{
+	X=7;
+	Y=8;
+	cout<<"Default constructor is called!";
+	displayxy();
+}
+
+Point::Point(int x)
+{
+	X=x;
+	Y=8;
+	cout<<"Constructor is called!";
+	displayxy(); 
+}
+Point::Point(int x,int y)
+{
+	X=x;
+	Y=y;
+	cout<<"Constructor is called!";
+	displayxy();
+}
+
+void Point::displayxy()
+{
+	cout<<"("<<X<<","<<Y<<")"<<endl;
+}
+
+int main()
+{
+	Point p1(3,4),p2[2]={5,6},p3;
+	//创建对象p1时调用有两个参数的构造函数
+	//对象p2为对象数组，每个数组元素被创建时都要调用构造函数
+	//创建对象p3时调用默认构造函数 
+	return 0;
+}
+
+
