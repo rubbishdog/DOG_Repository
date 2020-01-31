1023b9c4cc8dd8c0c9f2e73b8ccc1cc8eef6c88d
rubbishdog <761586470@qq.com>
2020-01-31 17:24:43 +0800

    构造函数和析构函数

--- code/PointClass.cpp
+++ code/PointClass.cpp
@@ -1,4 +1,4 @@
-//构造函数示例
+//构造函数和析构函数示例
 #include<iostream>
 using namespace std;
 //点类的界面部分 
@@ -7,6 +7,7 @@ class Point{
 		Point(); //默认构造函数 
 		Point(int); //一个参数的构造函数 
 		Point(int,int); //两个参数的构造函数 
+		~Point(); //析构函数 
 		void displayxy(); 
 	private:
 		int X,Y;
@@ -35,6 +36,12 @@ Point::Point(int x,int y)
 	displayxy();
 }
 
+Point::~Point()
+{
+	cout<<"Destructor is called!";
+	displayxy(); 
+}
+
 void Point::displayxy()
 {
 	cout<<"("<<X<<","<<Y<<")"<<endl;
@@ -43,6 +50,7 @@ void Point::displayxy()
 int main()
 {
 	Point p1(3,4),p2[2]={5,6},p3;
+	cout<<endl;
 	//创建对象p1时调用有两个参数的构造函数
 	//对象p2为对象数组，每个数组元素被创建时都要调用构造函数
 	//创建对象p3时调用默认构造函数 
